944905efa80ff60a2d7d1c0c9013c1880751017a
nyomilm <nyomilm@hotmail.com>
2019-07-10 22:44:13 -0500

    stretch update

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,4 +1,68 @@
+const title = document.getElementsByTagName("title")
+title[0].textContent = 'Digital Timer'
 
+function Timer(elem) {
+    var time = 0;
+    var interval;
+    var offset;
 
+    function update() {
+        if (this.isOn) {
+            time += delta();
+        }
+        var formattedTime = timeFormatter(time);
+        console.log(formattedTime);
+        elem.textContent = formattedTime;
+    }
 
+    function delta() {
+        var now = Date.now();
+        var timePassed = now - offset;
+        offset = now;
+        return timePassed;
+    }
+
+    function timeFormatter(timeInMilliseconds) {
+        var time = new Date(timeInMilliseconds)
+        var seconds = time.getSeconds().toString();
+        var milliseconds = time.getMilliseconds().toString();
+
+        if (seconds.length < 2) {
+            seconds = '0' + seconds
+        }
+        if (seconds == 10) {
+            watch.stop()
+            return seconds;
+        }
+        if (milliseconds.length < 2) {
+            milliseconds = '0' + milliseconds
+        }
+        return seconds + '.' + milliseconds;
+    }
+
+    this.isOn = false;
+
+    this.start = function () {
+        if (!this.isOn) {
+            interval = setInterval(update.bind(this), 10);
+            offset = Date.now();
+            this.isOn = true;
+        }
+    };
+
+    this.stop = function () {
+        if (this.isOn) {
+            clearInterval(interval);
+            interval = null;
+            this.isOn = false;
+        }
+    };
+
+    this.reset = function () {
+        if (!this.isOn) {
+            time = 0;
+            update();
+        }
+    };
+}
 
